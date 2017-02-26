c111eaa4922fb183c587540ae1716a88271b292d
Cristina Pérez <pem.cristina@gmail.com>
2017-02-26 17:12:04 +0100

    Tested changeBackgroundImage method

--- app/src/main/java/com/bquiet/bquiet/activities/MainActivity.java
+++ app/src/main/java/com/bquiet/bquiet/activities/MainActivity.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.bquiet.bquiet.R;
 import com.bquiet.bquiet.manager.RecorderManager;
 import com.github.anastr.speedviewlib.Speedometer;
 
 public class MainActivity extends AppCompatActivity {
+    public static final float HIGH_NOISE = 50;
+    public static final float MEDIUM_NOISE = 40;
+
 
     private Speedometer speedometer;
     private Speedometer speedometer2;
+
     private ImageButton playButton;
     private ImageButton pauseButton;
+    private LinearLayout layout;
 
     private boolean speedometreWithTremble = false;
 
@@ -30,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         //speedometer2 = (Speedometer) findViewById(R.id.awesomeSpeedometer);
         playButton = (ImageButton) findViewById(R.id.activity_main_play_button);
         pauseButton = (ImageButton) findViewById(R.id.activity_main_pause_button);
+        layout = (LinearLayout) findViewById(R.id.activity_main_layout);
 
         speedometer.setWithTremble(speedometreWithTremble);
 
@@ -42,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void newValueFromEar(double spl) {
                         speedometer.speedTo((float) spl);
+                        changeBackgroundImage((float) spl);
                         //speedometer2.speedTo((float) spl);
                     }
                 });
@@ -60,5 +68,15 @@ public class MainActivity extends AppCompatActivity {
                 ear.cancel(recorderManager.isListening());
             }
         });
+    }
+
+    private void changeBackgroundImage(float noiseLevel) {
+        if(noiseLevel < MEDIUM_NOISE) {
+            layout.setBackgroundResource(R.drawable.teacher_low_noise);
+        } else if(noiseLevel > MEDIUM_NOISE && noiseLevel < HIGH_NOISE) {
+            layout.setBackgroundResource(R.drawable.teacher_medium_noise);
+        } else if(noiseLevel > HIGH_NOISE) {
+            layout.setBackgroundResource(R.drawable.teacher_high_noise);
+        }
     }
 }
