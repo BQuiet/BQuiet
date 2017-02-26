f1949932e43279cee16d5c01af8555b0cf4db052
Cristina Pérez <pem.cristina@gmail.com>
2017-02-26 16:26:14 +0100

    Configured play and pause button

--- app/src/main/java/com/bquiet/bquiet/activities/MainActivity.java
+++ app/src/main/java/com/bquiet/bquiet/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.bquiet.bquiet.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.bquiet.bquiet.R;
 import com.bquiet.bquiet.manager.RecorderManager;
@@ -9,9 +11,15 @@ import com.github.anastr.speedviewlib.Speedometer;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static RecorderManager recorderManager = new RecorderManager();
     private Speedometer speedometer;
     private Speedometer speedometer2;
+    private ImageButton playButton;
+    private ImageButton pauseButton;
+
+    private boolean speedometreWithTremble = false;
+
+    RecorderManager recorderManager;
+    RecorderManager.Ear ear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,32 +27,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         speedometer = (Speedometer) findViewById(R.id.speedView);
-        speedometer2 = (Speedometer) findViewById(R.id.awesomeSpeedometer);
-    }
+        //speedometer2 = (Speedometer) findViewById(R.id.awesomeSpeedometer);
+        playButton = (ImageButton) findViewById(R.id.activity_main_play_button);
+        pauseButton = (ImageButton) findViewById(R.id.activity_main_pause_button);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+        speedometer.setWithTremble(speedometreWithTremble);
 
-        RecorderManager.Ear ear = recorderManager.new Ear();
-        ear.setOnEarListener(new RecorderManager.EarListener() {
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void newValueFromEar(double spl) {
-                speedometer.speedTo((float) spl);
-                speedometer2.speedTo((float) spl);
+            public void onClick(View view) {
+                recorderManager = new RecorderManager();
+                ear = recorderManager.new Ear();
+                ear.setOnEarListener(new RecorderManager.EarListener() {
+                    @Override
+                    public void newValueFromEar(double spl) {
+                        speedometer.speedTo((float) spl);
+                        //speedometer2.speedTo((float) spl);
+                    }
+                });
+                recorderManager.setListening(true);
+                //ear.cancel(recorderManager.isListening());
+                ear.execute();
             }
         });
-        recorderManager.setListening(true);
-        ear.execute();
 
-
-
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recorderManager.setListening(false);
+                speedometer.speedTo(0.0f);
+                recorderManager.stopMediaRecorder();
+                ear.cancel(recorderManager.isListening());
+            }
+        });
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        recorderManager.stop();
-    }
-
 }
