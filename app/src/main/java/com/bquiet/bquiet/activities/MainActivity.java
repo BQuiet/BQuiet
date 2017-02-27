8313a2c9f4a7623819b11344ee76addf40c6b22e
Angonmel <angonmel@gmail.com>
2017-02-27 11:43:34 +0100

    fragments package, tabhost and tabContent added with error in MainActivity [Error:(53, 50) error: cannot find symbol variable activity_main_layout ]

--- app/src/main/java/com/bquiet/bquiet/activities/MainActivity.java
+++ app/src/main/java/com/bquiet/bquiet/activities/MainActivity.java
@@ -1,36 +1,50 @@
 package com.bquiet.bquiet.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.bquiet.bquiet.R;
+import com.bquiet.bquiet.fragments.FragmentTab;
 import com.bquiet.bquiet.manager.RecorderManager;
 import com.github.anastr.speedviewlib.Speedometer;
 
-public class MainActivity extends AppCompatActivity {
-    public static final float HIGH_NOISE = 50;
-    public static final float MEDIUM_NOISE = 40;
+public class MainActivity extends FragmentActivity {
 
+    private FragmentTabHost mTabHost;
 
-    private Speedometer speedometer;
-    private Speedometer speedometer2;
-
-    private ImageButton playButton;
-    private ImageButton pauseButton;
-    private LinearLayout layout;
-
-    private boolean speedometreWithTremble = false;
-
-    RecorderManager recorderManager;
-    RecorderManager.Ear ear;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menubquiet, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        mTabHost = (FragmentTabHost) findViewById(R.id.activity_main_tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.activity_main__tabcontent);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("Noisemetre").setIndicator("Noisemetre", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("My Class").setIndicator("My Class", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("Noise Level").setIndicator("Noise Level", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("Graphics").setIndicator("Graphics", null),
+                FragmentTab.class, null);
 
         speedometer = (Speedometer) findViewById(R.id.speedView);
         //speedometer2 = (Speedometer) findViewById(R.id.awesomeSpeedometer);
@@ -69,6 +83,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
+
+    public static final float HIGH_NOISE = 50;
+    public static final float MEDIUM_NOISE = 40;
+
+
+    private Speedometer speedometer;
+    private Speedometer speedometer2;
+
+    private ImageButton playButton;
+    private ImageButton pauseButton;
+    private LinearLayout layout;
+
+    private boolean speedometreWithTremble = false;
+
+    RecorderManager recorderManager;
+    RecorderManager.Ear ear;
+
+
+
+
 
     private void changeBackgroundImage(float noiseLevel) {
         if(noiseLevel < MEDIUM_NOISE) {
