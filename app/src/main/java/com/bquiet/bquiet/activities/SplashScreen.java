27c1b185d8ba6d6481f78fca2acf6a0149435f57
Emi <emimlg1@gmail.com>
2017-03-01 13:26:09 +0100

    added fullscreen

--- app/src/main/java/com/bquiet/bquiet/activities/SplashScreen.java
+++ app/src/main/java/com/bquiet/bquiet/activities/SplashScreen.java
@@ -2,7 +2,6 @@ package com.bquiet.bquiet.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
 
@@ -20,8 +19,6 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Set portrait orientation
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         // Hide title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
