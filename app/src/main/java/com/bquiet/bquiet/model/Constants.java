ab0ee03e93483ae1beaac91702fa12609948ee2b
Cristina Pérez <pem.cristina@gmail.com>
2017-03-04 22:43:10 +0100

    Added Constants class with default speedometre labels

---
+++ app/src/main/java/com/bquiet/bquiet/model/Constants.java
@@ -0,0 +1,6 @@
+package com.bquiet.bquiet.model;
+
+public class Constants {
+    public static final int DEFAULT_LOW_LABEL_SPEEDOMETRE = 30;
+    public static final int DEFAULT_MEDIUM_LABEL_SPEEDOMETRE = 70;
+}
