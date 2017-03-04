a44e26abd802a95807d16818369803bb1862c9f3
Cristina Pérez <pem.cristina@gmail.com>
2017-03-04 20:35:24 +0100

    Tested noise level fragment. Now speedometre (noise level fragment) changes according to number pickers

--- app/src/main/java/com/bquiet/bquiet/fragments/NoiseLevelFragment.java
+++ app/src/main/java/com/bquiet/bquiet/fragments/NoiseLevelFragment.java
@@ -8,23 +8,58 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bquiet.bquiet.R;
+import com.github.anastr.speedviewlib.Speedometer;
+
+import pl.polak.clicknumberpicker.ClickNumberPickerListener;
+import pl.polak.clicknumberpicker.ClickNumberPickerView;
+import pl.polak.clicknumberpicker.PickerClickType;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NoiseLevelFragment extends Fragment {
 
+    Speedometer speedometer;
+
+    ClickNumberPickerView lowClickNumberPickerView;
+    ClickNumberPickerView highClickNumberPickerView;
 
     public NoiseLevelFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_noise_level, container, false);
+        View view = inflater.inflate(R.layout.fragment_noise_level, container, false);
+
+        speedometer = (Speedometer) view.findViewById(R.id.fragment_noise_level_speedometer);
+
+        lowClickNumberPickerView = (ClickNumberPickerView) view.findViewById(R.id.fragment_noise_level_low_picker);
+        highClickNumberPickerView = (ClickNumberPickerView) view.findViewById(R.id.fragment_noise_level_high_picker);
+
+        lowClickNumberPickerView.setPickerValue(MainFragment.LOW_NOISE);
+        highClickNumberPickerView.setPickerValue(MainFragment.MEDIUM_NOISE);
+
+        speedometer.setLowSpeedPercent(MainFragment.LOW_NOISE);
+        speedometer.setMediumSpeedPercent(MainFragment.MEDIUM_NOISE);
+
+        lowClickNumberPickerView.setClickNumberPickerListener(new ClickNumberPickerListener() {
+            @Override
+            public void onValueChange(float previousValue, float currentValue, PickerClickType pickerClickType) {
+                speedometer.setLowSpeedPercent((int) currentValue);
+            }
+        });
+
+        highClickNumberPickerView.setClickNumberPickerListener(new ClickNumberPickerListener() {
+            @Override
+            public void onValueChange(float previousValue, float currentValue, PickerClickType pickerClickType) {
+                speedometer.setMediumSpeedPercent((int) currentValue);
+            }
+        });
+
+        return view;
+
     }
 
 }
