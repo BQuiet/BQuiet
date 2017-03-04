3f79a42a66233e99813b6c8cc57b89337d34d351
Cristina Pérez <pem.cristina@gmail.com>
2017-03-04 22:30:21 +0100

    Saved low and high label with shared preferences in noise level fragment

--- app/src/main/java/com/bquiet/bquiet/fragments/NoiseLevelFragment.java
+++ app/src/main/java/com/bquiet/bquiet/fragments/NoiseLevelFragment.java
@@ -1,6 +1,7 @@
 package com.bquiet.bquiet.fragments;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -38,16 +39,25 @@ public class NoiseLevelFragment extends Fragment {
         lowClickNumberPickerView = (ClickNumberPickerView) view.findViewById(R.id.fragment_noise_level_low_picker);
         highClickNumberPickerView = (ClickNumberPickerView) view.findViewById(R.id.fragment_noise_level_high_picker);
 
-        lowClickNumberPickerView.setPickerValue(MainFragment.LOW_NOISE);
-        highClickNumberPickerView.setPickerValue(MainFragment.MEDIUM_NOISE);
+        SharedPreferences preferences = getActivity().getSharedPreferences("preferences", 0);
+        int lowMargin = preferences.getInt("lowMargin", 50);
+        int highMargin = preferences.getInt("highMargin", 90);
 
-        speedometer.setLowSpeedPercent(MainFragment.LOW_NOISE);
-        speedometer.setMediumSpeedPercent(MainFragment.MEDIUM_NOISE);
+        lowClickNumberPickerView.setPickerValue(lowMargin);
+        highClickNumberPickerView.setPickerValue(highMargin);
+
+        speedometer.setLowSpeedPercent(lowMargin);
+        speedometer.setMediumSpeedPercent(highMargin);
 
         lowClickNumberPickerView.setClickNumberPickerListener(new ClickNumberPickerListener() {
             @Override
             public void onValueChange(float previousValue, float currentValue, PickerClickType pickerClickType) {
                 speedometer.setLowSpeedPercent((int) currentValue);
+
+                SharedPreferences preferences = getActivity().getSharedPreferences("preferences", 0);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("lowMargin", (int) currentValue);
+                editor.commit();
             }
         });
 
@@ -55,6 +65,11 @@ public class NoiseLevelFragment extends Fragment {
             @Override
             public void onValueChange(float previousValue, float currentValue, PickerClickType pickerClickType) {
                 speedometer.setMediumSpeedPercent((int) currentValue);
+
+                SharedPreferences preferences = getActivity().getSharedPreferences("preferences", 0);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("highMargin", (int) currentValue);
+                editor.commit();
             }
         });
 
