b41638302ceb5db474553eff49698f5135ed6780
Cristina Pérez <pem.cristina@gmail.com>
2017-03-07 12:04:21 +0100

    Tested recycler view with false class

--- app/src/main/java/com/bquiet/bquiet/fragments/MyClassFragment.java
+++ app/src/main/java/com/bquiet/bquiet/fragments/MyClassFragment.java
@@ -3,28 +3,40 @@ package com.bquiet.bquiet.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bquiet.bquiet.MyClass;
 import com.bquiet.bquiet.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class MyClassFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MyClassFragment extends Fragment {
+    private RecyclerView recyclerView;
 
     public MyClassFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_class, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_my_class, container, false);
 
+        recyclerView = (RecyclerView) view.findViewById(R.id.fragment__my_class_recycler_view);
+
+        List<MyClass> listMyClass = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            MyClass e = new MyClass("MyClass" + i);
+            listMyClass.add(e);
+        }
+        MyClassListAdapter adapter = new MyClassListAdapter(getContext(), listMyClass);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
+    }
 }
