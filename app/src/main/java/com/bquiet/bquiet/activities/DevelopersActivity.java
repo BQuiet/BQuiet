2ceeae4ac5914a99f2d441bb5589c430aa596415
Emi <emimlg1@gmail.com>
2017-03-07 13:57:10 +0100

    added developers activity

---
+++ app/src/main/java/com/bquiet/bquiet/activities/DevelopersActivity.java
@@ -0,0 +1,13 @@
+package com.bquiet.bquiet.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DevelopersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developers);
+    }
+}
