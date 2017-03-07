73632b950f2fc40abf63a7ed02484b698b51f5e7
Emi <emimlg1@gmail.com>
2017-03-07 14:34:45 +0100

    added fixed aboutfragment

--- app/src/main/java/com/bquiet/bquiet/fragments/AboutFragment.java
+++ app/src/main/java/com/bquiet/bquiet/fragments/AboutFragment.java
@@ -1,18 +1,31 @@
 package com.bquiet.bquiet.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bquiet.bquiet.R;
+import com.bquiet.bquiet.activities.DevelopersActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutFragment extends Fragment {
+
+    @BindView(R.id.fragment_about_RN__Telephone)TextView telephone_robonautas;
+    @BindView(R.id.fragment_about_RN__Email)TextView email_robonautas;
+    @BindView(R.id.fragment_about_RN__web)TextView web_robonautas;
+    @BindView(R.id.fragment_about_BQ__gitHub)TextView adress_github_BQ;
+    @BindView(R.id.fragment_aboout___button_bquiet)ImageView button_developers;
 
 
     public AboutFragment() {
@@ -24,7 +37,50 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        ButterKnife.bind(this, view);
+
+        telephone_robonautas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        email_robonautas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        web_robonautas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button_developers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent developer = new Intent(getContext(), DevelopersActivity.class);
+                startActivity(developer);
+            }
+        });
+
+        adress_github_BQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        return view;
+
+
+
     }
 
 }
