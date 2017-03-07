e4dae41a626dcf05f6097cf676484ee1792289d2
Emi <emimlg1@gmail.com>
2017-03-07 13:57:42 +0100

    added developers activity

--- app/src/main/java/com/bquiet/bquiet/activities/AboutActivity.java
+++ app/src/main/java/com/bquiet/bquiet/activities/AboutActivity.java
@@ -1,12 +1,33 @@
 package com.bquiet.bquiet.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bquiet.bquiet.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AboutActivity extends AppCompatActivity {
+
+    @BindView(R.id.fragment_aboout___button_bquiet)ImageView button_developers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+
+        button_developers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent developer = new Intent(AboutActivity.this, DevelopersActivity.class);
+                startActivity(developer);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/bquiet/bquiet/activities/DevelopersActivity.java
+++ app/src/main/java/com/bquiet/bquiet/activities/DevelopersActivity.java
@@ -1,7 +1,9 @@
 package com.bquiet.bquiet.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bquiet.bquiet.R;
 
 public class DevelopersActivity extends AppCompatActivity {
 
