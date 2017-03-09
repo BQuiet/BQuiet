b29210f7ef9fde32f23fec9e0742e6ed6f599ae6
Alberto <al_loki@msn.com>
2017-03-09 09:52:39 +0100

    Added Log/send fragment & LoginFragment

--- app/src/main/java/com/bquiet/bquiet/fragments/LoginFragment.java
+++ app/src/main/java/com/bquiet/bquiet/fragments/LoginFragment.java
@@ -11,11 +11,15 @@ import android.widget.EditText;
 
 import com.bquiet.bquiet.R;
 
+import static android.view.View.GONE;
+
 public class LoginFragment extends Fragment {
 
     EditText userText;
     EditText passwordText;
     Button logInButton;
+    Button sendButton;
+    Button logOutButton;
 
     public LoginFragment() {
     }
@@ -28,8 +32,38 @@ public class LoginFragment extends Fragment {
         userText = (EditText) view.findViewById(R.id.fragment_login_user);
         passwordText = (EditText) view.findViewById(R.id.fragment_login_password);
         logInButton = (Button) view.findViewById(R.id.fragment_login_button);
+        sendButton = (Button) view.findViewById(R.id.fragment_login_send_button);
+        logOutButton = (Button) view.findViewById(R.id.fragment_login_logout_button);
+
+        logInButton.setVisibility(View.VISIBLE);
+        userText.setVisibility(View.VISIBLE);
+        passwordText.setVisibility(View.VISIBLE);
+        sendButton.setVisibility(GONE);
+        logOutButton.setVisibility(GONE);
+
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TODO: Meter esto en el metodo de comprobacion del login o token
+                logInButton.setVisibility(GONE);
+                userText.setVisibility(GONE);
+                passwordText.setVisibility(GONE);
+                sendButton.setVisibility(View.VISIBLE);
+                logOutButton.setVisibility(View.VISIBLE);
+
+
+            }
+        });
         
         return view;
+    }
+
+    public static Fragment newInstance() {
+        LoginFragment myFragment = new LoginFragment();
+        return myFragment;
+
     }
 
 }
